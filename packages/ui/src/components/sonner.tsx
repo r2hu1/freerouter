9b1baa07c79aece354a70d3ecb9b588cc1b41527
r2hu1 <newasram86@gmail.com>
2026-07-26 22:29:54 +0530

    style(ui): biome fmt sonner

--- packages/ui/src/components/sonner.tsx
+++ packages/ui/src/components/sonner.tsx
@@ -1,8 +1,14 @@
 "use client"
 
+import {
+  IconAlertOctagon,
+  IconAlertTriangle,
+  IconCircleCheck,
+  IconInfoCircle,
+  IconLoader,
+} from "@tabler/icons-react"
 import { useTheme } from "next-themes"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
-import { IconCircleCheck, IconInfoCircle, IconAlertTriangle, IconAlertOctagon, IconLoader } from "@tabler/icons-react"
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
@@ -12,21 +18,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{
-        success: (
-          <IconCircleCheck className="size-4" />
-        ),
-        info: (
-          <IconInfoCircle className="size-4" />
-        ),
-        warning: (
-          <IconAlertTriangle className="size-4" />
-        ),
-        error: (
-          <IconAlertOctagon className="size-4" />
-        ),
-        loading: (
-          <IconLoader className="size-4 animate-spin" />
-        ),
+        success: <IconCircleCheck className="size-4" />,
+        info: <IconInfoCircle className="size-4" />,
+        warning: <IconAlertTriangle className="size-4" />,
+        error: <IconAlertOctagon className="size-4" />,
+        loading: <IconLoader className="size-4 animate-spin" />,
       }}
       style={
         {
