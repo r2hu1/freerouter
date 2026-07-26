c46917229fba4c29909a80c5bd66eb247925094a
r2hu1 <newasram86@gmail.com>
2026-07-26 22:29:47 +0530

    style(ui): biome fmt field

--- packages/ui/src/components/field.tsx
+++ packages/ui/src/components/field.tsx
@@ -1,11 +1,11 @@
 "use client"
 
+import { type VariantProps, cva } from "class-variance-authority"
 import { useMemo } from "react"
-import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@workspace/ui/lib/utils"
 import { Label } from "@workspace/ui/components/label"
 import { Separator } from "@workspace/ui/components/separator"
+import { cn } from "@workspace/ui/lib/utils"
 
 function FieldSet({ className, ...props }: React.ComponentProps<"fieldset">) {
   return (
